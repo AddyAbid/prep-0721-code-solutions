12c3f1b6938d0b6e7255d85d2bd5bf15ceb20cf7
Addy Abid <addy.abeed@gmail.com>
2021-08-10 20:55:21 -0700

    Hours to Minutes and Get Greeting

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,20 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var converted = convertHoursToMinutes(2);
+console.log('sum', converted);
+
+function getGreeting(name) {
+  return name;
+}
+
+var greeting = getGreeting('World!');
+console.log('Hello', greeting);
